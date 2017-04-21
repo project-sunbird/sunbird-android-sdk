ea80c59a5266b61b65e362eb7194513f6b57422f
Shriharsh Saboji <shriharshs@gmail.com>
2017-04-21 13:04:50 +0530

    Issue #14 feat :  ProfileService class added

---
+++ genie-services-lib/src/main/java/org/ekstep/genieservices/profile/ProfileService.java
@@ -0,0 +1,17 @@
+package org.ekstep.genieservices.profile;
+
+import org.ekstep.genieservices.BaseService;
+import org.ekstep.genieservices.commons.AppContext;
+
+/**
+ * Any service related to profile will be called using ProfileService
+ *
+ * @author shriharsh
+ */
+
+public class ProfileService extends BaseService {
+
+    public ProfileService(AppContext appContext) {
+        super(appContext);
+    }
+}
