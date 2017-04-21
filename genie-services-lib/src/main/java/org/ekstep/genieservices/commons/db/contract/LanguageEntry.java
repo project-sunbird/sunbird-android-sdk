bde76124d1ef5c45491b9a5e2ba1753a61d002d7
Mathew Pallan <mathew.pallan@tarento.com>
2017-04-21 10:03:02 +0530

    Issue #9 chore : Making non-instantiatable classes abstract

--- genie-services-lib/src/main/java/org/ekstep/genieservices/commons/db/contract/LanguageEntry.java
+++ genie-services-lib/src/main/java/org/ekstep/genieservices/commons/db/contract/LanguageEntry.java
@@ -3,7 +3,7 @@ package org.ekstep.genieservices.commons.db.contract;
 import org.ekstep.genieservices.commons.db.BaseColumns;
 import org.ekstep.genieservices.commons.db.DbConstants;
 
-public class LanguageEntry implements BaseColumns {
+public abstract class LanguageEntry implements BaseColumns {
 
     public static final String TABLE_NAME = "languages";
     public static final String COLUMN_NAME_NAME = "name";
