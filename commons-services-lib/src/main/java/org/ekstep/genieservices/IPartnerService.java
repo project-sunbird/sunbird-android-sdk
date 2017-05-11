43d3cd02fb595231ada7bed4e83a80ea1d961414
Shriharsh Saboji <shriharshs@gmail.com>
2017-05-11 16:35:47 +0530

    Issue #26 feat : Partner Service Javadoc comments refactoring

--- commons-services-lib/src/main/java/org/ekstep/genieservices/IPartnerService.java
+++ commons-services-lib/src/main/java/org/ekstep/genieservices/IPartnerService.java
@@ -4,7 +4,7 @@ import org.ekstep.genieservices.commons.bean.GenieResponse;
 import org.ekstep.genieservices.commons.bean.PartnerData;
 
 /**
- * IPartnerService is the interface with all the required APIs for performing partner related
+ * This is the interface with all the required APIs for performing partner related
  * operations.
  */
 public interface IPartnerService {
@@ -12,9 +12,9 @@ public interface IPartnerService {
     /**
      * This api is used to register a partner with the specified partnerID and publicKey in PartnerData.
      * <p>
-     * <p>In case of a success response, response.getStatus() will return TRUE and response.getMessage() will return the string "successful"
+     * <p>On successful registering the partner, the response will return status as TRUE and with "successful" message.
      * <p>
-     * <p>In case of a failed response, response.getStatus() will return FALSE and response.getError() could return one of the following:
+     * <p>On failing to register the partner, the response will return status as FALSE and the errors could be one of the following:
      * <p>MISSING_PARTNER_ID - partnerID is not provided</p>
      * <p>MISSING_PUBLIC_KEY - publicKey is not provided</p>
      * <p>INVALID_RSA_PUBLIC_KEY - publicKey is not as per specs</p>
@@ -28,7 +28,7 @@ public interface IPartnerService {
     /**
      * This api is used to check if the partnerId is already registered.
      * <p>
-     * <p>Response will always be success response, but response.getResult() in Boolean type will return TRUE if the partner is registered
+     * <p>Response status always be True, with result type as Boolean and will return TRUE if the partner is registered
      * or FALSE if the partner is not registered.
      *
      * @param partnerID
@@ -39,10 +39,10 @@ public interface IPartnerService {
     /**
      * This api is used to start the Partner Session with the specified partnerID in PartnerData.
      * <p>
-     * <p>In case of a success response, response.getStatus() will return TRUE and response.getMessage() will return the string "successful"
+     * <p>On successful starting the session, the response will have status as TRUE and message as "successful".
      * <p>
      * <p>
-     * <p>In case of a failed response, response.getStatus() will return FALSE and response.getError() will return the following:
+     * <p>On failing to start the session, the response will have status as FALSE with the following error.
      * <p>UNREGISTERED_PARTNER - Session start failed! Partner: <partnerId> </p>
      *
      * @param partnerData Required to get the related PartnerData to start the session
@@ -53,11 +53,11 @@ public interface IPartnerService {
     /**
      * This api is used to terminate the Partner Session with the specified partnerID.
      * <p>
-     * <p>In case of a success response, response.getStatus() will return TRUE and response.getMessage() will return the string "successful"
+     * <p>On successful terminating the session, the response will have status as TRUE and message as "successful"
      * <p>
      * <p>
-     * <p>In case of a failed response, response.getStatus() will return FALSE and response.getError() will return the following:
-     * <p>UNREGISTERED_PARTNER - Session termination failed! Partner: <partnerId> </p>
+     * <p>On failing to start the session, the response will have status as FALSE with the following error.
+     * <p>UNREGISTERED_PARTNER - Session start failed! Partner: <partnerId> </p>
      *
      * @param partnerData Required to get the related PartnerData to start the session
      * @return {@link GenieResponse<Void>}
@@ -67,12 +67,9 @@ public interface IPartnerService {
     /**
      * This api is used to send Partner data specified in PartnerData. This api will encrypt the partnerData with the publicKey given earlier during registration.
      * <p>
-     * <p>In case of a success response, response.getStatus() will return TRUE and response.getResult() will return the response string.
+     * <p>On successful sending the data, the response will have status as TRUE and result can be fetched from the response.
      * <p>
-     * <p> In case of a failed response, response.getError() could return one
-     * of the following:
-     * <p>
-     * <p>
+     * <p>On failing to send data, the response will return status as FALSE and the errors could be one of the following:
      * <p>UNREGISTERED_PARTNER - Sending data failed! Partner: <partnerId> </p>
      * <p>ENCRYPTION_FAILURE - Encrypting data failed! Partner: <partnerId> </p>
      *
