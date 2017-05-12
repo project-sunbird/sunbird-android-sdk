da1b9861763b64c228649a229f0ea855a04b7fa7
Shriharsh Saboji <shriharshs@gmail.com>
2017-05-12 11:53:40 +0530

    Issue #26 feat : Telemetry Service Javadoc comments

--- commons-services-lib/src/main/java/org/ekstep/genieservices/ITelemetryService.java
+++ commons-services-lib/src/main/java/org/ekstep/genieservices/ITelemetryService.java
@@ -9,9 +9,30 @@ import org.ekstep.genieservices.commons.bean.telemetry.BaseTelemetry;
 
 public interface ITelemetryService {
 
+    /**
+     * This api will save the telemetry details passed to it as String.
+     * <p>
+     * <p> On successful saving the telemetry, the response will return status as TRUE and with "Event Saved Successfully" message.
+     * <p>
+     * <p>On failing to save the telemetry details, the response will return status as FALSE and the error be the following:
+     * <p>PROCESSING_ERROR
+     *
+     * @param eventString
+     * @return {@link GenieResponse<Void>}
+     */
     GenieResponse<Void> saveTelemetry(String eventString);
 
+    /**
+     * This api will save the telemetry details passed to it as {@link BaseTelemetry}.
+     * <p>
+     * <p> On successful saving the telemetry, the response will return status as TRUE and with "Event Saved Successfully" message.
+     * <p>
+     * <p>On failing to save the telemetry details, the response will return status as FALSE and the error be the following:
+     * <p>PROCESSING_ERROR
+     *
+     * @param event
+     * @return {@link GenieResponse<Void>}
+     */
     GenieResponse<Void> saveTelemetry(BaseTelemetry event);
-
 
 }
