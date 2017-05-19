9c8a31b4708089438f152121e81566183e20d38d
sneha.krishnamurthy <sneha.krishnamurthy@tarento.com>
2017-05-19 17:53:32 +0530

    Issue #34 test: Check Partner service entire flow for multiple partner

--- genie-services/src/androidTest/java/org/ekstep/genieservices/PartnerService/PartnerServiceTest.java
+++ genie-services/src/androidTest/java/org/ekstep/genieservices/PartnerService/PartnerServiceTest.java
@@ -57,4 +57,44 @@ public class PartnerServiceTest extends GenieServiceTestBase {
 
         //TODO : Check if telemetry events are logged.
     }
+
+    @Test
+    public void _2shouldTestEntirePartnerFlowForMultiplePartner() {
+
+        PartnerData partner1Data = new PartnerData("org.ekstep.partner", "1.7", "PARTNER1_ID", "partner_1 data", PUBLIC_KEY);
+
+        GenieResponse registerPartner1Response = activity.registerPartner(partner1Data);
+        Assert.assertTrue(registerPartner1Response.getStatus());
+        checkIfTelemetryEventIsLogged();
+
+        GenieResponse startPartner1Session = activity.startPartnerSession(partner1Data);
+        Assert.assertTrue(startPartner1Session.getStatus());
+        checkIfTelemetryEventIsLogged();
+
+        PartnerData partner2Data = new PartnerData("org.ekstep.partner", "1.8", "PARTNER2_ID", "partner_2 data", PUBLIC_KEY);
+
+        GenieResponse registerPartner2Response = activity.registerPartner(partner2Data);
+        Assert.assertTrue(registerPartner2Response.getStatus());
+        checkIfTelemetryEventIsLogged();
+
+        GenieResponse startPartner2Session = activity.startPartnerSession(partner2Data);
+        Assert.assertTrue(startPartner2Session.getStatus());
+        checkIfTelemetryEventIsLogged();
+
+        GenieResponse sendPartner1Data = activity.sendData(partner1Data);
+        Assert.assertTrue(sendPartner1Data.getStatus());
+        checkIfTelemetryEventIsLogged();
+
+        GenieResponse sendPartner2Data = activity.sendData(partner2Data);
+        Assert.assertTrue(sendPartner2Data.getStatus());
+        checkIfTelemetryEventIsLogged();
+
+        GenieResponse terminatePartner1Data = activity.terminatePartnerSession(partner1Data);
+        Assert.assertTrue(terminatePartner1Data.getStatus());
+        checkIfTelemetryEventIsLogged();
+
+        GenieResponse terminatePartner2Data = activity.terminatePartnerSession(partner2Data);
+        Assert.assertTrue(terminatePartner2Data.getStatus());
+        checkIfTelemetryEventIsLogged();
+    }
 }
