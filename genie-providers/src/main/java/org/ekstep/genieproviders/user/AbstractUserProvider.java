866640b8090b5f1b9f59a10411d9e1b2925341d9
Shriharsh Saboji <shriharshs@gmail.com>
2017-05-19 14:16:02 +0530

    Issue #29 feat : Abstract content providers for Telemetry and User are added

---
+++ genie-providers/src/main/java/org/ekstep/genieproviders/user/AbstractUserProvider.java
@@ -0,0 +1,39 @@
+package org.ekstep.genieproviders.user;
+
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created on 19/5/17.
+ * shriharsh
+ */
+
+public abstract class AbstractUserProvider extends ContentProvider {
+    @Override
+    public abstract boolean onCreate();
+
+    @Override
+    public abstract Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder);
+
+    @Override
+    public abstract String getType(@NonNull Uri uri);
+
+    @Override
+    public abstract Uri insert(@NonNull Uri uri, @Nullable ContentValues values);
+
+    @Override
+    public abstract int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs);
+
+    @Override
+    public abstract int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs);
+
+    public String getCompleteUserAuthority(String authority) {
+        String fullAuthorityName = String.format("%s.profiles", authority);
+        return fullAuthorityName;
+    }
+}
