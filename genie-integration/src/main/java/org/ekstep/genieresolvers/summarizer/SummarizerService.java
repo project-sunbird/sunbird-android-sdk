67c78f28b8a427f2a2ffe4fd58eeafc2e9f1c991
Shriharsh Saboji <shriharshs@gmail.com>
2017-06-21 18:07:44 +0530

    Issue #42 fix : HierarchyData is received in the form of List<HierarchyInfo> now

--- genie-integration/src/main/java/org/ekstep/genieresolvers/summarizer/SummarizerService.java
+++ genie-integration/src/main/java/org/ekstep/genieresolvers/summarizer/SummarizerService.java
@@ -4,6 +4,11 @@ import android.content.Context;
 
 import org.ekstep.genieresolvers.BaseService;
 import org.ekstep.genieservices.commons.IResponseHandler;
+import org.ekstep.genieservices.commons.bean.HierarchyInfo;
+import org.ekstep.genieservices.commons.utils.StringUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created on 6/6/17.
@@ -14,14 +19,23 @@ public class SummarizerService extends BaseService {
 
     private String appQualifier;
     private Context context;
+    private String hierarchyDataInString;
 
     public SummarizerService(Context context, String appQualifier) {
         this.context = context;
         this.appQualifier = appQualifier;
     }
 
-    public void getLearnerAssessment(String uid, String contentId, String hierarchyData, IResponseHandler responseHandler) {
-        LearnerAssessmentTask createUserTask = new LearnerAssessmentTask(context, appQualifier, uid, contentId, hierarchyData);
+    public void getLearnerAssessment(String uid, String contentId, List<HierarchyInfo> hierarchyData, IResponseHandler responseHandler) {
+        List<String> idList = new ArrayList<>();
+        if (hierarchyData != null) {
+            for (HierarchyInfo hierarchyInfo : hierarchyData) {
+                idList.add(hierarchyInfo.getIdentifier());
+            }
+
+            hierarchyDataInString = StringUtil.join("/", idList);
+        }
+        LearnerAssessmentTask createUserTask = new LearnerAssessmentTask(context, appQualifier, uid, contentId, hierarchyDataInString);
         createAndExecuteTask(responseHandler, createUserTask);
     }
 
