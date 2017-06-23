a984fc364d187d50ea1f349a690d213a4110e9b7
Mathew Pallan <mathew.pallan@tarento.com>
2017-06-23 18:51:16 +0530

    Issue #8 chore: Added audience filter attribute and mode for content player

--- genie-services/src/main/java/org/ekstep/genieservices/utils/ContentPlayer.java
+++ genie-services/src/main/java/org/ekstep/genieservices/utils/ContentPlayer.java
@@ -70,7 +70,7 @@ public class ContentPlayer {
         }
 
         intent.putExtra(ServiceConstants.BundleKey.BUNDLE_KEY_ORIGIN, "Genie");
-        intent.putExtra(ServiceConstants.BundleKey.BUNDLE_KEY_MODE, "Play");
+        intent.putExtra(ServiceConstants.BundleKey.BUNDLE_KEY_MODE, "play");
         if (content.getHierarchyInfo() != null)
             intent.putExtra(ServiceConstants.BundleKey.BUNDLE_KEY_CONTENT_EXTRAS, GsonUtil.toJson(content.getHierarchyInfo()));
         intent.putExtra(ServiceConstants.BundleKey.BUNDLE_KEY_APP_INFO, GsonUtil.toJson(contentData));
